5672858503164f7bb42aa2fa42bfb614aa8719d0
Olivier Doucet <odoucet@oxeva.fr>
2023-07-11 16:33:57 +0200

    helmupdate: use comment instead of helm edition

--- helmupdate.py
+++ helmupdate.py
@@ -1,15 +1,16 @@
 import requests
 import argparse
 import ruamel.yaml
+from ruamel.yaml.comments import CommentedMap
+import datetime
 
 ARTIFACTHUB_API_URL = "https://artifacthub.io/api/v1/packages/helm/{}"
 
-def get_latest_version(chart):
+def get_package_info(chart):
     url = ARTIFACTHUB_API_URL.format(chart)
     response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
-        return data["version"]
+        return response.json()
     return None
 
 def update_helmfile(helmfile_path):
@@ -27,9 +28,16 @@ def update_helmfile(helmfile_path):
         chart = release.get("chart")
         if chart:
             current_version = release.get("version")
-            latest_version = get_latest_version(chart)
+            packageInfo = get_package_info(chart)
+            if packageInfo:
+                latest_version = packageInfo["version"]
+            else:
+                latest_version = None
+
             if latest_version and current_version != latest_version:
-                release["version"] = latest_version
+                datetime_object = datetime.datetime.fromtimestamp(packageInfo['ts'])
+                release = CommentedMap(release)
+                release.yaml_set_comment_before_after_key("version", before=f"Latest version: {latest_version} - released on {datetime_object.strftime('%Y-%m-%d')}")
         updated_releases.append(release)
 
     updated_helmfile = {
@@ -37,6 +45,10 @@ def update_helmfile(helmfile_path):
         "helmDefaults": helm_defaults,
         "releases": updated_releases,
     }
+
+    updated_helmfile = CommentedMap(updated_helmfile)
+    datetime_object = datetime.datetime.now()
+    updated_helmfile.yaml_set_comment_before_after_key("releases", before=f"Last version scan: {datetime_object.strftime('%Y-%m-%d')}")
 
     with open(helmfile_path, "w") as f:
         yaml.dump(updated_helmfile, f)
